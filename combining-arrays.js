00174ff577cbb4795e662b8ef4bc5d986d2687b1
Nishath-Mumtahin <mumtahinnishath@gmail.com>
2025-02-15 08:19:30 +0600

    added combining-arrays file

---
+++ combining-arrays.js
@@ -0,0 +1,11 @@
+/*  
+1.Create two arrays of your choice.
+2.Use the concat method to combine the two arrays into a new array.
+3.Print both the original arrays and the combined array using console.log().
+*/
+const array1 = ['Mango', 'Guava', 'Orange'];
+const array2 = [1, 2, 3];
+
+const combinedArray = array1.concat(array2);
+
+console.log(combinedArray);
--- isArray.js
+++ isArray.js
@@ -3,7 +3,7 @@
 2.Now use isArray to check if each variable is an array.
 3.Print a message to the console indicating whether each variable is an array or not.
 */
-const arrayValue = ['1', '2', '3'];
+const arrayValue = [1, 2, 3];
 const nonarrayValue = 'I am non-array';
 
 console.log(Array.isArray(arrayValue));
