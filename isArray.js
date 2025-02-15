39713fd126afc88b46d2fc63724ddc41fbd6f44a
Nishath-Mumtahin <mumtahinnishath@gmail.com>
2025-02-15 08:11:02 +0600

    added isArray file

---
+++ isArray.js
@@ -0,0 +1,11 @@
+/*
+1.Create different variables, each containing either an array or a non-array value.
+2.Now use isArray to check if each variable is an array.
+3.Print a message to the console indicating whether each variable is an array or not.
+*/
+const arrayValue = ['1', '2', '3'];
+const nonarrayValue = 'I am non-array';
+
+console.log(Array.isArray(arrayValue));
+
+console.log(Array.isArray(nonarrayValue));
